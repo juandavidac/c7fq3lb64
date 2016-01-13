1b5f38b87c7d76b254916f2daecdefdd83e6d786
juandavidac <jd.ac@outlook.com>
2016-01-13 11:39:37 -0500

    crea filtros

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,23 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:category_id].blank? && params[:concept].blank?
+      @expenses = Expense.order("date DESC")
+    elsif params[:concept] && params[:category_id].blank?
+      @expenses=Expense.where(concept: "#{params[:concept]}").to_a
+    elsif params[:category_id] && params[:concept].blank?
+      @category=Category.find(params[:category_id])
+      @expenses=@category.expenses.all
+    elsif params[:concept] && params[:category_id]
+      @category=Category.find(params[:category_id])
+      @expensescategory=@category.expenses.all
+      @expensesconcept=Expense.where(concept: "#{params[:concept]}").to_a
+      @expenses=(@expensescategory|@expensesconcept).sort {|a,b| b.date <=> a.date}
+      #@expenses=@expensesunordered.sort {|a,b| b.date <=> a.date}
+    else
+      @expenses = Expense.order("date DESC")
+    end
   end
 end
+
+#  elsif params[:category_id].nil? || params[:concept].nil?
+#    @expenses = Expense.order("date DESC")
