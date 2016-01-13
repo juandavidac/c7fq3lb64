1df1a90ec5a7f84b7b3af0a2d1be76255bd7fd98
juandavidac <jd.ac@outlook.com>
2016-01-13 13:51:58 -0500

    agrega info en la barra

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,14 +4,17 @@ class ExpensesController < ApplicationController
       @expenses = Expense.order("date DESC")
     elsif params[:concept] && params[:category_id].blank?
       @expenses=Expense.where(concept: "#{params[:concept]}").to_a
+      @current=params[:concept]
     elsif params[:category_id] && params[:concept].blank?
       @category=Category.find(params[:category_id])
       @expenses=@category.expenses.all
+      @current=@category.name
     elsif params[:concept] && params[:category_id]
       @category=Category.find(params[:category_id])
       @expensescategory=@category.expenses.all
       @expensesconcept=Expense.where(concept: "#{params[:concept]}").to_a
       @expenses=(@expensescategory|@expensesconcept).sort {|a,b| b.date <=> a.date}
+      @current=@category.name+", "+params[:concept]
       #@expenses=@expensesunordered.sort {|a,b| b.date <=> a.date}
     else
       @expenses = Expense.order("date DESC")
